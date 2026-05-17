92240cf5c72a5945f818aa9855ceb0fbb8730272
Anonymous9109 <rayenmoussa509@gmail.com>
2026-05-17 20:28:16 +0100

    Update player-data.js

--- Series/player-data.js
+++ Series/player-data.js
@@ -92,7 +92,7 @@ window.videoData = {
   "From-S4E4-subs": [
     { label: "Arabic", lang: "ar", src: "../subtitles/From-S4E2-Arabic.txt" }
   ],
-
+"From-S4E5":"https://dl.dropboxusercontent.com/scl/fi/e0tvu2elg424gluqn8pad/FROM-S01E05.mp4?rlkey=0ecodvu6pjd0uxqstlt825wh8&st=2o5149no&dl=1",
   "WidowsBay-S1E1": "https://dl.dropboxusercontent.com/scl/fi/rczh6tfszhc8pcdeytc1z/Widow-s-Bay-_-S1E1-2026.mp4?rlkey=fbzcga70849wvszjzchcl6vlj&st=0lmup05y&dl=1",
   "WidowsBay-S1E1-subs": [
     { "label": "English", "lang": "en", "src": "../subtitles/WidowsBay/WidowsBay-S1E1-English.txt" },
