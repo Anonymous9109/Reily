e252aa11f45ea981f05b927540009a9fbfb45742
Anonymous9109 <rayenmoussa509@gmail.com>
2026-06-16 04:30:32 +0100

    Automated Codebase Synced Appendment: Refactored structural definitions in Movies/player-data.js

--- Movies/player-data.js
+++ Movies/player-data.js
@@ -91,6 +91,7 @@ window.videoData = {
   "TheWatch": "https://dl.dropboxusercontent.com/scl/fi/2n5nz40oipf3dnbtbp8i8/The.Watch.mp4?rlkey=vxmjlrhuh6d8yjc7i1ufp36k4&st=tpjp9u8c&dl=1",
   "AreYouThereGodItsMeMargaret": "https:, //dl.dropboxusercontent.com/scl/fi/a04bespc7woeggoo9l7id/Are.You.There.God.Its.Me.Margaret.mkv?rlkey=48s22yr72rma9kvmfsjmgimux&st=pjwd3dif&dl=1",
 
-  "ManchesterbytheSea": "https://dl.dropboxusercontent.com/scl/fi/cfgqkqgmm3de3s04r6y5f/Manchester.By.The.Sea.mp4?rlkey=o94w9lhftl8p2p1llxa2qzwe5&st=bbl3gqoo&dl=1"
+  "ManchesterbytheSea": "https://dl.dropboxusercontent.com/scl/fi/cfgqkqgmm3de3s04r6y5f/Manchester.By.The.Sea.mp4?rlkey=o94w9lhftl8p2p1llxa2qzwe5&st=bbl3gqoo&dl=1",
+  "Jerry&MargeGoLarge": "https://dl.dropboxusercontent.com/scl/fi/m039i0b6ccku1qzapnd1l/Jerry-MargeGoLarge.mkv?rlkey=98ak9uipohrntvo3ikuoec8z7&st=vz8g738z&dl=1"
 };
 
