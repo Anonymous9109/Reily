e970d8318bad0a87c5f923142262121447fbf974
Anonymous9109 <rayenmoussa509@gmail.com>
2026-06-18 19:55:51 +0100

    Automated Codebase Synced Appendment: Refactored structural definitions in Movies/player-data.js

--- Movies/player-data.js
+++ Movies/player-data.js
@@ -100,6 +100,7 @@ window.videoData = {
   "WhatYouWishFor": "https://dl.dropboxusercontent.com/scl/fi/pp5xjsmva5j8r9fvoq44c/What.You.Wish.For.mkv?rlkey=stip0ffydkqyt0aejfdnct5mz&st=uzxc84r4&dl=1",
   "PiratesoftheCaribbeanTheCurseoftheBlackPearl": "https://dl.dropboxusercontent.com/scl/fi/3lcz7cqm9zgauk4yguu35/Pirates.of.the.Caribbean.Curse.of.the.Black.Pearl.mkv?rlkey=rzx0es836iu35x4evpzbb4nhb&st=ylu8aqws&dl=1",
   "TheHungerGames": "https://dl.dropboxusercontent.com/scl/fi/cjiyvko62r9pk8mtnwjym/The.Hunger.Games.mkv?rlkey=0dhel8kvpjmjmgoiiuij0rppm&st=iotmkl1f&dl=1",
-  "Maternalinstinct": "https://dl.dropboxusercontent.com/scl/fi/akwck0v7zd0mbdtq2ovip/Maternal.Instinct.mp4?rlkey=csb4oopvuibehazbrkue93s3h&st=p5i4roao&dl=1"
+  "Maternalinstinct": "https://dl.dropboxusercontent.com/scl/fi/akwck0v7zd0mbdtq2ovip/Maternal.Instinct.mp4?rlkey=csb4oopvuibehazbrkue93s3h&st=p5i4roao&dl=1",
+  "PiratesoftheCaribbeanDeadMansChest": "https://dl.dropboxusercontent.com/scl/fi/0k5ah7agaz3pj4kuna1rz/Pirates.of.the.Caribbean.Dead.Man-s.Chest.mkv?rlkey=0kk4isd69c3gvg8gmgydfkvmr&st=n8jg4l5v&dl=1"
 };
 
