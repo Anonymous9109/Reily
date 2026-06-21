a20479dd78a49d8339929e888bd6604f16e65925
Anonymous9109 <rayenmoussa509@gmail.com>
2026-06-21 18:39:58 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,26 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
+// Config mapping for background operations
 firebase.initializeApp({
   apiKey: "AIzaSyCExki28m1NNepVQEIjmcQzR8z8O68LqIc",
+  authDomain: "rinolski-notifications.firebaseapp.com",
   projectId: "rinolski-notifications",
-  appId: "1:355554579853:web:3caa961653c0cdc0a359c4",
+  messagingSenderId: "355554579853",
+  appId: "1:355554579853:web:3caa961653c0cdc0a359c4"
 });
 
 const messaging = firebase.messaging();
 
-// This handles background notifications
+// Launches native tray alerts when message arrives and browser tab is closed
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
+  console.log('Received background notification payload: ', payload);
+  
+  const notificationTitle = payload.notification.title || "Rinolski Alert";
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/firebase-logo.png'
+    body: payload.notification.body || "New event occurred.",
+    icon: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' // fall-back webicon placeholder
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
