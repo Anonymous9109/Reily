f55840d0ec64e80846bc5bbe6faabbff5d686a18
Anonymous9109 <rayenmoussa509@gmail.com>
2026-07-30 19:31:37 +0100

    Update script.js

--- Movies/script.js
+++ Movies/script.js
@@ -616,7 +616,7 @@ function goBack() {
       border: none;
       border-radius: 12px;
       padding: 10px 0;
-      margin-top: 24px;
+      margin-top: 100px;
       color: var(--rev-text-main);
       box-sizing: border-box;
       text-align: left !important;
